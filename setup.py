9b90c646b9ed02ffa6da93be5f2262b181f9b73b
Sanamansoorii <129357174+Sanamansoorii@users.noreply.github.com>
2024-06-30 20:08:46 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='my_custom_env',
     version='0.0.1',
     install_requires=['gym', 'numpy'],
-    packages=['my_custom_env', 'my_custom_env.envs'],
+    # packages=['my_custom_env', 'my_custom_env.envs'],
+    packages = find_packages()
 )
